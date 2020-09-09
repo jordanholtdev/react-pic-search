5025fe2ab0773b794cd6f172aa72cbc104d37a1b
J Holt <jordan.su@protonmail.com>
2020-09-09 17:06:50 -0400

    💥 Initial commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: '' };
+
+  // the arrow function will bind the this value to instances of SearchBar
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    
+    this.props.onSubmit(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
